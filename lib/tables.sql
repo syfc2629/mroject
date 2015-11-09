21fbd38600707f35f2c23aeb7ea67b736fe799be
violinbg <violin.developer@gmail.com>
2015-11-09 11:08:54 -0500

    Added new tables

--- lib/tables.sql
+++ lib/tables.sql
@@ -1,23 +1,79 @@
------------------
--- USERS TABLE --
------------------
-CREATE TABLE IF NOT EXISTS `users` (
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2015 at 05:08 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mroject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `prjid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  PRIMARY KEY (`prjid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projectusers`
+--
+
+CREATE TABLE IF NOT EXISTS `projectusers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `role` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `prjid` int(11) NOT NULL,
+  `roleid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
-ALTER TABLE `users` ADD PRIMARY KEY (`userid`);
+--
+-- Table structure for table `roles`
+--
 
-ALTER TABLE `users`  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `roleid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
---------------------
--- SESSIONS TABLE --
---------------------
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`roleid`, `name`) VALUES
+(1, 'Manager'),
+(2, 'Developer'),
+(3, 'Tester'),
+(4, 'Release Engineer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
 
 CREATE TABLE IF NOT EXISTS `sessions` (
   `sessionid` varchar(20) NOT NULL,
@@ -26,4 +82,77 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `validuntil` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
+  `statusid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`statusid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`statusid`, `name`) VALUES
+(1, 'In research'),
+(2, 'In progress'),
+(3, 'In testing'),
+(4, 'In release'),
+(5, 'Completed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `taskid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `assignedto` int(11) NOT NULL,
+  `duedate` datetime NOT NULL,
+  `progress` int(11) NOT NULL,
+  `statusid` int(11) NOT NULL,
+  PRIMARY KEY (`taskid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasksnotes`
+--
+
+CREATE TABLE IF NOT EXISTS `tasksnotes` (
+  `noteid` int(11) NOT NULL AUTO_INCREMENT,
+  `taskid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `dateadded` datetime NOT NULL,
+  `note` varchar(1000) NOT NULL,
+  PRIMARY KEY (`noteid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
